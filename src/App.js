10014e0bd7be6c4a5ca995b8b97293556f60a16c
Kasra Bigdeli <kasra85@gmail.com>
2019-01-22 21:40:32 -0800

    Added CapRover files

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            React deployed using CapRover!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://caprover.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            CapRover.com
           </a>
         </header>
       </div>
